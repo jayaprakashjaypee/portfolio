be3773a542ed976596d7c2cc93bbd6ad11b4ec6f
jayaprakashjaypee <jayaprakash.m786@gmail.com>
2022-12-01 16:13:17 +0530

    image updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Typewriter from "typewriter-effect";
+import Typewriter from "typewriter-effect"; 
 
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
@@ -30,10 +30,10 @@ const Home = () => {
             <Link
               to="portfolio"
               smooth
-              duration={500}
+              duration={400}
               className="group text-gray-900 font-bold w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-rcursor-pointer hover:bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 hover:text-gray-600"
             >
-              Portfolio
+              {Portfolio}
               <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
